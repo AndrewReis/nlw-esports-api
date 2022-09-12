e2cee481830cd14b2740c87796056a7d2d4443ff
andrewReis <andrew-reii@outlook.com>
2022-09-12 10:14:15 -0300

    start project

---
+++ src/server.mjs
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { logger } from './utils/logger.mjs'
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+  return response.json([{ok: true}])
+})
+
+app.listen(3333, () => logger.info('Server running at 3333'))
